6f5af772f8d84cba45994ddbfd825ef2cfb1a2a2
justsanchez <alvarosanchez9261@gmail.com>
2024-08-29 00:28:57 -0400

    initial build

---
+++ sunset_app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    // Hello world but centered
+    <div className="flex justify-center items-center h-screen">
+      <p>Hello World!</p>
+    </div>
+  );
+}
